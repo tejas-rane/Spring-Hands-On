1484222e25f19f31e2d0f226f34449f00c286b59
Tejas Rane <tejas.m.rane@gmail.com>
2017-11-10 18:35:55 -0800

    setter injection

--- src/org/springcore/Restaurant.java
+++ src/org/springcore/Restaurant.java
@@ -3,8 +3,13 @@ package org.springcore;
 public class Restaurant {
 	HotDrink hotDrink;
 
-	public Restaurant(HotDrink hotDrink) {
+	/*public Restaurant(HotDrink hotDrink) {
 		super();
+		this.hotDrink = hotDrink;
+	}*/
+	
+	public void setHotDrink(HotDrink hotDrink) {
+		
 		this.hotDrink = hotDrink;
 	}
 	public void prepareHotDrink(){
